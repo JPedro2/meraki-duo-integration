8f98ac4afcb87743ab210405aab42ee65e17f0cf
Joshua Dean <joshualukedean@gmail.com>
2020-07-01 11:27:40 +0100

    added april login

--- controllers/app.controllers.js
+++ controllers/app.controllers.js
@@ -1,4 +1,6 @@
 const Duo = require('../node_modules/@duosecurity/duo_web/index'); 
+const fetch = require('node-fetch');
+const btoa = require('btoa');
 //Render the First Page 
 exports.signOn = (req, res) => {
 
@@ -21,17 +23,46 @@ exports.stageTwo = (req,res) => {
 
     //Grab the username from the signin form submitted
     username = req.body.username
+    password = req.body.password
 
-    //Build the signrequest with the .env and the username
-    const sigRequest = Duo.sign_request(iKey, sKey, aKey, username);
+    //Encode the usernames and passwords
+    let to_encode = username+":"+password;
+    let base64_encoded = btoa(to_encode);
 
-    //create an object to hold the information to pass to the front end
-    let userInformation = {
-        duoHost : host,
-        duoSig : sigRequest.toString() 
-    }
+    //Check Credentials against april
+    fetch(process.env.baseUrlAuth + "/api/auth/token",{
+        method: 'GET',
+        headers: {
+            "Authorization" : "Basic "+base64_encoded,
+            "Access-Control-Allow-Origin" : "*" 
+        }
+    })
+    .then(function(response) {
+        if(response.status === 200) { //If the login credentials are okay
+            
+            //Build the signrequest with the .env and the username
+            const sigRequest = Duo.sign_request(iKey, sKey, aKey, username);
+            
+            //create an object to hold the information to pass to the front end
+            let userInformation = {
+                duoHost : host,
+                duoSig : sigRequest.toString() 
+            }
 
-    //render the front end with the information above templated in
-    res.render('stage-two', userInformation);
+            console.log("yes")
+           
+            
+            //render the front end with the information above templated in
+            res.render('stage-two', userInformation);
+        } else { //If there is an issue with the login credentials
+            
+            //send them back to stage one with an error flag
+            res.render('sign-on', {error: true});
+            
+            console.log("no")
+        }
+    }).catch(err => {
+        console.log(err);
+    })
 }
 
